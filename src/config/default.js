f573d539616e0517d5130a96ec287e0738ff55f5
“gaowei1012” <mingwei2222@126.com>
2020-07-10 17:22:05 +0800

    fix mysql connection data name

--- src/config/default.js
+++ src/config/default.js
@@ -4,7 +4,7 @@ const config = {
     port: 8080,
     host: '127.0.0.1',
     database: {
-        DATABASE: 'falllove',
+        DATABASE: 'fallLove',
         USERNAME: 'root',
         PASSWORD: 'gao129231wei',
         PORT: '3306',
