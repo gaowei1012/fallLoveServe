9c3393a3d1016d6c9d71dc798375792dc332a384
“gaowei1012” <mingwei2222@126.com>
2020-07-29 14:02:26 +0800

    修改接口单词拼写错误

--- src/route/album.js
+++ src/route/album.js
@@ -2,8 +2,8 @@ const router = require('koa-router')()
 const controller = require('../controller/album')
 
 router.post('/api/album/getAllPhoto', controller.getAllPhoto)
-router.post('/api/album/createAlbumFile', controller.createAlbumFile)
-router.post('/api/album/deleteAPhoto', controller.deleteAPhoto)
+router.post('/api/album/createlbumFile', controller.createAlbumFile)
+router.post('/api/album/deletePhoto', controller.deleteAPhoto)
 router.post('/api/album/getPhotoList', controller.getPhotoList)
 router.post('/api/album/uploadPhoto', controller.uploadAlbum)
 
