8210773143b4b4cf216bf61b9130a0b8e730133b
gaowei1012 <mingwei2222@126.com>
2020-10-08 16:53:39 +0800

    fix article list

--- src/controller/articles.js
+++ src/controller/articles.js
@@ -1,199 +1,241 @@
 
+const { ne } = require('sequelize/types/lib/operators');
 const UserModal = require('../db/mysql')
-/**
- * @swagger
- *
- * /createArticle/:
- *   
- * post:
- *     description: get all users
- *     produces:
- *       - application/json
- *     responses:
- *       200:
- *         description: get all users
- */
-exports.createArticle = async ctx => {
+/* 发表文章 */
+exports.createArticle = async (ctx, next) => {
     const {title,content} = ctx.request.body;
     let create_at = new Date()
     // 发表时间点暂时放在后端
-    await UserModal.insterArticleData([title,content,create_at])
-        .then(result => {
+    try {
+        if ((title !== undefined) && (content !== undefined)) {
+            await UserModal.insterArticleData([title,content,create_at])
+                .then(result => {
+                    ctx.body = {
+                        code: 1,
+                        message: '发表成功'
+                    }
+                })
+                .catch(err => {
+                    ctx.body = {
+                        code: -1,
+                        message: '发表失败'
+                    }
+                })
+        } else {
             ctx.body = {
-                code: 200,
-                message: '发表成功'
+                code: -2,
+                message: '必传参数不能为空'
             }
-        })
-        .catch(err => {
-            ctx.body = {
-                code: 500,
-                message: '发表失败'
-            }
-        })
+        }
+    } catch(err) {
+        Promise.reject(err)
+    }
+
+    await next()
 }
 
 
-/**
- * @swagger
- *
- * /deleteArticle/:
- *   
- * post:
- *     description: get all users
- *     produces:
- *       - application/json
- *     responses:
- *       200:
- *         description: get all users
- */
-exports.deleteArticle = async ctx => {
+/* 删除文章 */
+exports.deleteArticle = async (ctx, next) => {
     const {id} = ctx.request.body;
-    await UserModal.deleteArticleById(id)
-        .then(result => {
+    try {
+        if (id !== undefined) {
+            await UserModal.deleteArticleById(id)
+                .then(result => {
+                    ctx.body = {
+                        code: 1,
+                        message: 'ok',
+                        data: '删除成功!'
+                    }
+                })
+                .catch(err => {
+                    ctx.body = {
+                        code: -1,
+                        message: `删除失败:${err}`
+                    }
+                })
+        } else {
             ctx.body = {
-                code: 200,
-                message: 'ok',
-                data: '删除成功!'
+                code: -2,
+                message: 'id不能为空!'
             }
-        })
-        .catch(err => {
-            ctx.body = {
-                code: 500,
-                message: `删除失败:${err}`
-            }
-        })
+        }
+        
+    } catch (err) {
+        Promise.reject(err)
+    }
+
+    await next()
 }
 
-/**
- * @swagger
- *
- * /eitdArticle/:
- *   
- * post:
- *     description: get all users
- *     produces:
- *       - application/json
- *     responses:
- *       200:
- *         description: get all users
- */
-exports.eitdArticle = async ctx => {
+/* 编辑文章 */
+exports.eitdArticle = async (ctx, next) => {
    const {title,content,id} = ctx.request.body;
-   await UserModal.updateArticleById(title,content,id)
-     const result = {
-         code: 1,
-         message: '请求成功',
-         data: []
-     }
-    ctx.body = result
+   try {
+        if ((title !== undefined) && (content !== undefined) && (id !== undefined)) {
+            await UserModal.updateArticleById(title,content,id)
+                .then(ret => {
+                    ctx.body = {
+                        code: 1,
+                        message: 'Ok',
+                        data: '更新成功!'
+                    }
+                }).catch(err => {
+                    ctx.body = {
+                        code: -1,
+                        message: '更新失败'
+                    }
+                })
+        } else {
+            const result = {
+                code: -2,
+                message: '查询失败，参数不能为空！',
+            }
+            ctx.body = result
+        }
+   } catch (err) {
+       Promise.reject(err)
+   }
+
+   await next()
 }
 /**
  * 通过id查找文章
- * @param {*} ctx 
  */
-exports.findeArticleData = async ctx => {
+exports.findeArticleData = async (ctx, next) => {
     const {id} = ctx.request.body;
     try {
         if (id !== undefined) {
             await UserModal.findArticelById(id)
-            .then(result => {
-                ctx.body = {
-                    code: 200,
-                    message: 'Ok',
-                    data: result[0]
-                }
-            })
-            .catch(err => {
-                ctx.body = {
-                    code: 500,
-                    message: `ERROR: ${err}`
-                }
+                .then(result => {
+                    ctx.body = {
+                        code: 1,
+                        message: 'Ok',
+                        data: result[0]
+                    }
+                })
+                .catch(err => {
+                    ctx.body = {
+                        code: -1,
+                        message: `ERROR: ${err}`
+                    }
             })
         } else {
             ctx.body = {
-                stateCode: 400,
+                code: -2,
                 message: 'id不能为空'
             }
         }
     } catch (err) {
         Promise.reject(err)
     }
+
+    await next()
 }
 
 /**
  * 获取所有文章
  */
-exports.searchArticle = async ctx => {
+exports.searchArticle = async (ctx, next) => {
     await UserModal.findArticelAll()
         .then(result => {
             ctx.body = {
-                code: 200,
-                message: '成功',
+                code: 1,
+                message: 'Ok',
                 data: result
             }
         })
         .catch(err => {
             ctx.body = {
-                code: 500,
-                message: '失败',
-                data: err,
+                code: -1,
+                message: '获取失败'
             }
         })
+
+    await next()
 }
 
 /**
  * 发表评论
  */
-exports.createComment = async ctx => {
+exports.createComment = async (ctx, next) => {
     const {content,avatar,username} = ctx.request.body;
     let nowData = new Date();
-    await UserModal.insterCommentData([content,avatar,username,nowData])
-        .then(result => {
+    try {
+        if ((content !== undefined) && (avatar !== undefined) && (username !== undefined)) {
+            await UserModal.insterCommentData([content,avatar,username,nowData])
+                .then(result => {
+                    ctx.body = {
+                        code: 1,
+                        message: '发表成功',
+                    }
+                })
+                .catch(err => {
+                    ctx.body = {
+                        code: -1,
+                        message: `发表失败：${err}`
+                    }
+                })
+        } else {
             ctx.body = {
-                code: 200,
-                message: '发表成功',
+                code: -2,
+                message: '参数不能为空',
             }
-        })
-        .catch(err => {
-            ctx.body = {
-                code: 500,
-                message: `发表失败：${err}`
-            }
-        })
+        }
+    } catch (err) {
+        Promise.reject(err)
+    }
+
+    await next()
 }
 
 // 查看文章评论
-exports.getArticleComment= async ctx => {
+exports.getArticleComment= async (ctx, next) => {
     await UserModal.findComment()
         .then(result => {
             ctx.body = {
-                code: 200,
-                message: 'ok',
+                code: 1,
+                message: 'Ok',
                 data: result
             }
         })
         .catch(err => {
             ctx.body = {
-                code: 500,
+                code: -1,
                 message: `err: ${err}`
             }
         })
+
+    await next()
 }
 
 // 删除评论
-exports.deleteCommentById=async ctx => {
+exports.deleteCommentById= async (ctx, next) => {
     const {id} = ctx.request.body;
-    await UserModal.deleteCommentById(id)
-        .then(result => {
+    try {
+        if (id !== undefined) {
+            await UserModal.deleteCommentById(id)
+                .then(result => {
+                    ctx.body = {
+                        code: 1,
+                        message: '删除成功'
+                    }
+                })
+                .catch(err => {
+                    ctx.body = {
+                        code: -1,
+                        message: '删除失败'
+                    }
+                })
+        } else {
             ctx.body = {
-                code: 200,
-                message: '删除成功'
+                code: -2,
+                message: '参数不能为空'
             }
-        })
-        .catch(err => {
-            ctx.body = {
-                code: 500,
-                message: '删除失败'
-            }
-        })
+        }
+    } catch (err) {
+        Promise.reject(err)
+    }
+
+    await next()
 }
