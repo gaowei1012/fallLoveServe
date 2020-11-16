8171fa12488c9e69fb2ec21fe547afe397be3538
执念 <mingwei2222@126.com>
2020-11-16 17:22:36 +0800

    Update default.js

--- src/config/default.js
+++ src/config/default.js
@@ -16,14 +16,14 @@ const config = {
     },
     oss: { // 阿里云oss对象存储
         region: 'oss-cn-hangzhou',  // 阿里云对象存储域名
-        accessKeyId: 'LTAI4GBo2qD2JUPaio8LJ6id', // api 接口id
-        accessKeySecret: 'T6Ztoz3U9cyk2oLumuNJukiJP26umw', // api 接口密码
+        accessKeyId: '*********', // api 接口id
+        accessKeySecret: '*******', // api 接口密码
         bucket: 'registiation', // bucket 名称
         url: 'https://registiation.oss-cn-hangzhou.aliyuncs.com/images/'
     },
     wx: {
-        appId: 'wx150f87bd2b50fc5d',
-        appSecret: 'dc650535105bdd7a3888fa9176a0460b'
+        appId: '*******',
+        appSecret: '************'
     },
     // 配置七牛云
     accessKey: '',
